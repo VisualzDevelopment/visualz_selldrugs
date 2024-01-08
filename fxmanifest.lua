e3d6ea7e88e68b159ca13abed39299cd58a4d2ec
Phillip Eis <45538799+PhillipEis@users.noreply.github.com>
2024-01-08 16:29:15 +0100

    finalize

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -11,12 +11,19 @@ version '1.0.0'
 shared_scripts {
   '@ox_lib/init.lua',
   '@es_extended/imports.lua',
-  'config/*.lua'
+  'config/config.lua'
 }
 
 client_script 'client/*.lua'
 
 server_scripts {
   '@oxmysql/lib/MySQL.lua',
+  'config/logs.lua',
   'server/*.lua',
+}
+
+escrow_ignore {
+  'config/*.lua',
+  'server/functions.lua',
+  'client/functions.lua',
 }
